1f4913bf7a83873ca1e5ac91b7d06d937d4ba708
Petrik van der Velde <petrikvandervelde@gmail.com>
2017-01-24 09:06:24 +1300

    Deleting unused files.
    references #2

--- src/New-Image.ps1
+++
@@ -1,3 +0,0 @@
-# Script to invoke packer with the provided configuration
-# Restore the cookbook dependencies -> cd cookbooks/<COOKBOOKNAME>; berks vendor ../../vendor/cookbooks
-# Invoke packer -> packer build -force .\<packer_config_file.json>
