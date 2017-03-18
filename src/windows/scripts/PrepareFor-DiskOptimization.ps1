32e1eedec299c450ed2327c68dd1a7ee933db74c
Petrik van der Velde <petrikvandervelde@gmail.com>
2017-03-19 11:38:20 +1300

    Adding improved logging to the diskoptimization and the sysprep. Also running sysprep in delayed mode so that we can return a zero exit code to packer
    references #2

--- src/windows/scripts/PrepareFor-DiskOptimization.ps1
+++ src/windows/scripts/PrepareFor-DiskOptimization.ps1
@@ -11,34 +11,49 @@
 [CmdletBinding()]
 param()
 
-Write-Host "Removing temp folders"
+function LogWrite
+{
+    param (
+        [string] $logText,
+        [string] $logFile
+    )
+
+    $now = Get-Date -format s
+
+    Add-Content -Path $logfile -Value "$now $logText"
+    Write-Host $logstring
+}
+
+$filePath = "$($env:TEMP)\$($MyInvocation.MyCommand.Name).started.txt"
+
+LogWrite -logFile $filePath -logText "Removing temp folders"
 $tempfolders = @("C:\Windows\Temp\*", "C:\Windows\Prefetch\*", "C:\Documents and Settings\*\Local Settings\temp\*", "C:\Users\*\Appdata\Local\Temp\*")
 Remove-Item $tempfolders -ErrorAction SilentlyContinue -Force -Recurse
 
 if ($SkipWindowsUpdates)
 {
-    Write-Host "Skipping cleanup"
+    LogWrite -logFile $filePath -logText "Skipping cleanup"
     exit 0
 }
 
 try
 {
-    Write-Host "Dism.exe /online /Cleanup-Image /StartComponentCleanup /ResetBase"
+    LogWrite -logFile $filePath -logText "Dism.exe /online /Cleanup-Image /StartComponentCleanup /ResetBase"
     Dism.exe /online /Cleanup-Image /StartComponentCleanup /ResetBase
 }
 catch
 {
-    Write-Host "Unable to reset base. Should be ok if patches have been slipstreamed."
+    LogWrite -logFile $filePath -logText "Unable to reset base. Should be ok if patches have been slipstreamed."
 }
 
 $moduleExist = Get-Module servermanager
 
 if ($moduleExist)
 {
-    Write-Host 'Get-WindowsFeature | ? { $_.InstallState -eq "Available" } | Uninstall-WindowsFeature -Remove'
+    LogWrite -logFile $filePath -logText 'Get-WindowsFeature | ? { $_.InstallState -eq "Available" } | Uninstall-WindowsFeature -Remove'
 
     import-module servermanager
-    Get-WindowsFeature | ? { $_.InstallState -eq 'Available' } | Uninstall-WindowsFeature -Remove
+    Get-WindowsFeature | Where-Object { $_.InstallState -eq 'Available' } | Uninstall-WindowsFeature -Remove
 }
 
 exit 0
--- src/windows/scripts/run-sysprep.ps1
+++ src/windows/scripts/run-sysprep.ps1
@@ -1,28 +1,85 @@
 $ProgressPreference="SilentlyContinue"
 
-@('c:\unattend.xml', 'c:\windows\panther\unattend\unattend.xml', 'c:\windows\panther\unattend.xml', 'c:\windows\system32\sysprep\unattend.xml') | %{
+
+function LogWrite
+{
+    param (
+        [string] $logText,
+        [string] $logFile
+    )
+
+    $now = Get-Date -format s
+
+    Add-Content -Path $logfile -Value "$now $logText"
+    Write-Host $logstring
+}
+
+$filePath = "$($env:TEMP)\$($MyInvocation.MyCommand.Name).started.txt"
+LogWrite -logFile $filePath -logText "Starting $($MyInvocation.MyCommand.Name)"
+
+@('c:\unattend.xml', 'c:\windows\panther\unattend\unattend.xml', 'c:\windows\panther\unattend.xml', 'c:\windows\system32\sysprep\unattend.xml') | Foreach-Object {
     if (test-path $_)
     {
-        write-host "Removing $($_)"
-        remove-item $_ > $null
+        LogWrite -logFile $filePath -logText "Removing $($_)"
+        try
+        {
+            remove-item $_ > $null
+        }
+        catch
+        {
+            LogWrite -logFile $filePath -logText "$($_.Exception.ToString())"
+        }
     }
 }
 
-if (!(test-path 'c:\windows\panther\unattend'))
+$path = 'c:\windows\panther\unattend'
+if (!(test-path $path))
 {
-    write-host "Creating directory $($_)"
-    New-Item -path 'c:\windows\panther\unattend' -type directory > $null
+    LogWrite -logFile $filePath -logText "Creating directory $path"
+    try
+    {
+        New-Item -path $path -type directory > $null
+    }
+    catch
+    {
+        LogWrite -logFile $filePath -logText "$($_.Exception.ToString())"
+    }
 }
 
 if (Test-Path 'e:\sysprep-unattend.xml')
 {
-    write-host "Copying e:\sysprep-unattend.xml to c:\windows\panther\unattend\unattend.xml"
-    Copy-Item 'e:\sysprep-unattend.xml' 'c:\windows\panther\unattend\unattend.xml' > $null
+    LogWrite -logFile $filePath -logText "Copying e:\sysprep-unattend.xml to c:\windows\panther\unattend\unattend.xml"
+    try
+    {
+        Copy-Item 'e:\sysprep-unattend.xml' 'c:\windows\panther\unattend\unattend.xml' > $null
+    }
+    catch
+    {
+        LogWrite -logFile $filePath -logText "$($_.Exception.ToString())"
+    }
 }
 else
 {
-    write-host "Copying f:\sysprep-unattend.xml to c:\windows\panther\unattend\unattend.xml"
-    Copy-Item 'f:\sysprep-unattend.xml' 'c:\windows\panther\unattend\unattend.xml'> $null
+    LogWrite -logFile $filePath -logText "Copying f:\sysprep-unattend.xml to c:\windows\panther\unattend\unattend.xml"
+    try
+    {
+        Copy-Item 'f:\sysprep-unattend.xml' 'c:\windows\panther\unattend\unattend.xml'> $null
+    }
+    catch
+    {
+        LogWrite -logFile $filePath -logText "$($_.Exception.ToString())"
+    }
 }
 
-& "c:\windows\system32\sysprep\sysprep.exe" /generalize /oobe /quiet /shutdown /unattend:'c:\windows\panther\unattend\unattend.xml'
+# Don't exit during sysprep because Packer doesn't notice that the VM is stopped. So run sysprep, then return an exit code
+# which Packer does understand?
+$command = '& "c:\windows\system32\sysprep\sysprep.exe" /generalize /oobe /quiet /quit /unattend:"c:\windows\panther\unattend\unattend.xml"'
+LogWrite -logFile $filePath -logText "Invoking sysprep with command: $($command)"
+Invoke-Expression -Command $command
+
+LogWrite -logFile $filePath -logText "Running shutdown"
+& shutdown -s -t 30
+LogWrite -logFile $filePath -logText "shutdown exit code was $LASTEXITCODE"
+
+LogWrite -logFile $filePath -logText "Return exit 0"
+exit 0
