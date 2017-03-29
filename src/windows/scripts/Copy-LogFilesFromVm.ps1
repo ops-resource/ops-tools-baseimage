a64f0ed38b9fa536297e53bc2b6a56fc9d1b847b
Petrik van der Velde <petrikvandervelde@gmail.com>
2017-03-29 22:36:10 +1300

    Now copies the directories but not the content
    references #2

--- src/windows/scripts/Copy-LogFilesFromVm.ps1
+++ src/windows/scripts/Copy-LogFilesFromVm.ps1
@@ -10,12 +10,12 @@ $cred = New-object System.Management.Automation.PSCredential $userName, $passwor
 $session = New-PSSession -VMName $vmName -Credential $cred
 
 $pathsToCopy = @{
-    'c:\windows\temp' = 'windows'
-    "c:\user\$($userName)\AppData\Local\Temp" = 'user'
+    'c:\windows\temp\*' = 'windows'
+    "c:\users\$($userName)\AppData\Local\Temp\*" = 'user'
 }
 
 foreach($path in $pathsToCopy.GetEnumerator())
 {
     $destination = Join-Path $targetDirectory $path.Value
-    Copy-item -FromSession $session -Path $path.Key -Destination $destination -Verbose
+    Copy-item -FromSession $session -Path $path.Key -Destination $destination -Verbose -ErrorAction SilentlyContinue -Recurse
 }
