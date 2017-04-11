6e37ef2055ed9a0dca5748c9ded1885abfbed2b8
Petrik van der Velde <petrikvandervelde@gmail.com>
2017-04-11 22:31:42 +1200

    Logging in with the default Administrator user. And updating the setupcomplete script to delete more stuff
    references #2

--- src/windows/scripts/setupcomplete.cmd
+++ src/windows/scripts/setupcomplete.cmd
@@ -1,2 +1,11 @@
-del /Q /F c:\windows\system32\sysprep\unattend.xml
-del /Q /F c:\windows\panther\unattend.xml
+rem Delete all the unattend install / panther / sysprep files
+rmdir /Q /S %systemroot%\windows\system32\sysprep\panther
+del /Q /F %systemroot%\windows\system32\sysprep\unattend.xml
+
+rmdir /Q /S %systemroot%\windows\panther
+
+rem Nuke the temp directories
+rmdir /Q /S %systemroot%\windows\Temp
+
+rem Finally delete the currently running script, we don't need it anymore
+del /F /Q %systemroot%\windows\setup\scripts\SetUpComplete.cmd
