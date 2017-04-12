c35260cb59b4ca863a273a6dd7939066be695aa7
Petrik van der Velde <petrikvandervelde@gmail.com>
2017-04-12 21:44:19 +1200

    Tweaking the unattend scripts
    references #2

--- src/windows/scripts/setupcomplete.cmd
+++ src/windows/scripts/setupcomplete.cmd
@@ -1,11 +1,11 @@
 rem Delete all the unattend install / panther / sysprep files
-rmdir /Q /S %systemroot%\windows\system32\sysprep\panther
-del /Q /F %systemroot%\windows\system32\sysprep\unattend.xml
+rmdir /Q /S %systemroot%\system32\sysprep\panther
+del /Q /F %systemroot%\system32\sysprep\unattend.xml
 
-rmdir /Q /S %systemroot%\windows\panther
+rmdir /Q /S %systemroot%\panther
 
 rem Nuke the temp directories
-rmdir /Q /S %systemroot%\windows\Temp
+rmdir /Q /S %systemroot%\Temp
 
 rem Finally delete the currently running script, we don't need it anymore
-del /F /Q %systemroot%\windows\setup\scripts\SetUpComplete.cmd
+del /F /Q %systemroot%\setup\scripts\SetUpComplete.cmd
